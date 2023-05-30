1c204818c40d0dfef3ad629df568cf3d57a4d53d
mrmrs <hi@mrmrs.cc>
2023-05-30 13:49:42 -0700

    Adds updated homepage

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
 import randomColor from 'random-hex-color'
 import chroma from 'chroma-js'
 import {sample} from 'lodash'
+import PoissonDiskSampling from 'poisson-disk-sampling'
 import { randomInt } from '../lib/random'
 
+
+function generateCoordinates(xMin, xMax, yMin, yMax, length) {
+  const coordinates = Array.from({length}, () => {
+    const x1 = xMin + Math.random() * (xMax - xMin);
+    const y1 = yMin + Math.random() * (yMax - yMin);
+    const x2 = xMin + Math.random() * (xMax - xMin);
+    const y2 = yMin + Math.random() * (yMax - yMin);
+    return { x1, y1, x2, y2 };
+  });
+
+  return coordinates;
+}
 function generateGeometricPalette() {
   const palette = [];
   
@@ -32,6 +45,7 @@ function generateGeometricPalette() {
   
   return palette;
 }
+
 const messages = [
 "Click or tap anywhere to continue...",
 "A color appears",
@@ -46,28 +60,31 @@ const messages = [
 ]
 
 export default function Home() {
-  const height = 4096
-  const width = 4096
-  const gridUnits = [4, 8, 16, 24, 32, 48, 64]
+  const size = useWindowSize()
+  const height = size.height
+  const width = size.width
+  const gridUnits = [2, 4, 8, 16, 24, 32, 48, 64, 128]
+  const strokeWidthArray = [0,1,2,3,6,8,16,32,64,128]
   const [generatedDesignCount, setGeneratedDesignCount] = useState(0)
   const [messageIndex, setMessageIndex] = useState(0)
 
   const [gridUnit, setGridUnit] = useState(gridUnits[randomInt(0,gridUnits.length-1)])
-  const [rows, setRows] = useState(randomInt(2,16))
-  const [cols, setCols] = useState(randomInt(2,16))
+  const [rows, setRows] = useState(parseInt(Number(1920) / Number(gridUnit)))
+  const [cols, setCols] = useState(parseInt(Number(2560) / Number(gridUnit)))
   const [symmetrical, setSymmetrical] = useState(false)
   const [cellWidth, setCellWidth] = useState(1000/cols)
   const [cellHeight, setCellHeight] = useState(1400/rows)
 
-  // Book 1 & 2
-  const [bgColor, setBgColor] = useState('white')
-  const [textColor, setTextColor] = useState('black')
+  const [baseColor, setBaseColor] = useState(randomColor())
+  const [bgColor, setBgColor] = useState('black')
+  const [textColor, setTextColor] = useState('white')
   const [bgColor2, setBgColor2] = useState(randomColor())
   const [maxLimit, setMaxLimit] = useState(randomInt(50,400))
   const [strokeWidth, setStrokeWidth] = useState(sample([4,4,4,4,6,6,6,6,6,8,8,16,64]))
   const [colorsInt, setColorsInt] = useState(randomInt(0,100))
   const [mode, setMode] = useState(sample(['lab', 'lch']))
   const [palette, setPalette] = useState( generateGeometricPalette())
+  const [coords, setCoords] = useState(generateCoordinates(0,width,0,height,128))
 
   const regenerateClick = () => {
     const newCount = generatedDesignCount +1
@@ -84,16 +101,14 @@ export default function Home() {
 
     //grid
     setGridUnit(gridUnits[randomInt(0,gridUnits.length-1)])
-    setRows(randomInt(1,32))
-    setCols(randomInt(1,32))
-    const baseColor = randomColor()
+    setRows(parseInt(Number(1920) / Number(gridUnit)))
+    setCols(parseInt(Number(2560) / Number(gridUnit)))
+    setBaseColor(randomColor())
     setColorsInt(randomInt(1,100))
     const newPalette = sample([
-      //chroma.scale([randomColor(), randomColor(), randomColor()]).mode(mode).colors(32),
-      //chroma.scale([randomColor(), randomColor(), randomColor()]).mode(mode).colors(32),
-      //chroma.scale([randomColor(), randomColor(), randomColor()]).mode(mode).colors(32),
-      //chroma.scale([randomColor(), baseColor, randomColor(), randomColor()]).mode(mode).colors(16),
-      //chroma.scale([chroma(baseColor).darken(4), baseColor, chroma(baseColor).brighten(4)]).mode(mode).colors(16),
+      chroma.scale([randomColor(), randomColor()]).mode(mode).colors(16),
+      chroma.scale([randomColor(), randomColor(), randomColor()]).mode(mode).colors(16),
+      chroma.scale([chroma(baseColor).darken(4), baseColor, chroma(baseColor).brighten(4)]).mode(mode).colors(16),
       //chroma.scale([chroma(baseColor).brighten(4), baseColor, chroma(baseColor).darken(4)]).mode(mode).colors(16),
       //chroma.scale(['#ff4f4f', baseColor, '#644fff']).mode(mode).colors(16),
       //chroma.scale([chroma('hsla(208,100%,10%,1)'), chroma('hsla(216,100%,90%,1)')]).mode(mode).colors(16),
@@ -111,45 +126,65 @@ export default function Home() {
 
     ])
     setMode(sample(['lch', 'lab']))
+    //setPalette(
+    //  colorsInt > 94?
+    //  [
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //    baseColor,
+    //  ] : [
+    //    newPalette[0],
+    //    newPalette[1],
+    //    newPalette[2],
+    //    newPalette[3],
+    //    newPalette[4],
+    //    newPalette[5],
+    //    newPalette[6],
+    //    newPalette[7],
+    //    newPalette[8],
+    //    newPalette[9],
+    //    newPalette[10],
+    //    newPalette[11],
+    //    newPalette[12],
+    //    newPalette[13],
+    //    newPalette[14],
+    //    newPalette[15],
+    //  ]
+    //)
+  }
+
+  const regenerateColor = () => {
+    setPalette(generateGeometricPalette())
+  }
+  const regenerateRandomColors = () => {
     setPalette(
-      colorsInt > 94?
-      [
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-        baseColor,
-      ] : [
-        newPalette[0],
-        newPalette[1],
-        newPalette[2],
-        newPalette[3],
-        newPalette[4],
-        newPalette[5],
-        newPalette[6],
-        newPalette[7],
-        newPalette[8],
-        newPalette[9],
-        newPalette[10],
-        newPalette[11],
-        newPalette[12],
-        newPalette[13],
-        newPalette[14],
-        newPalette[15],
-      ]
+      chroma.scale([randomColor(), randomColor(), randomColor()]).mode(mode).colors(16),
     )
   }
+  const regenerateMonochrome = () => {
+      setPalette(chroma.scale([chroma(baseColor).darken(4), baseColor, chroma(baseColor).brighten(4)]).mode(mode).colors(16))
+  }
+
+
+  const actions = [
+    <button onClick={regenerateMonochrome} children='Generate Monochrome Palette' />,
+    <button onClick={regenerateColor} children='Generate Palette' />,
+    <button onClick={regenerateRandomColors} children='Random Colors Palette' />,
+  ]
+
 
   return (
     <>
@@ -169,9 +204,10 @@ export default function Home() {
      zIndex: 9,
 
  }}>
-   <kbd style={{ padding: '16px', height: '64px', width: '96px', color: 'white', background: 'black', display: 'none', alignItems: 'center', justifyContent: 'center' }}>{generatedDesignCount}</kbd> 
+   <kbd style={{ padding: '16px', color: 'white', background: 'black', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'absolute', bottom: 0, right: 0,  }}>{generatedDesignCount}</kbd> 
 
-   <p className='transitions' style={{ lineHeight: 1.5, fontFamily: 'georgia', fontSize: '32px', margin: 0, flexGrow: 1, padding: '16px', color: textColor, maxWidth: '80ch' }}>{messages[messageIndex]}</p>
+   <p className='' style={{ lineHeight: 1.5, fontFamily: 'georgia', fontSize: '32px', margin: 0, flexGrow: 1, padding: '16px', color: textColor, maxWidth: '80ch' }}><span style={{ background: 'black', color: 'white'}}>{messages[messageIndex] + ' ' + width + 'x' + height}</span></p>
+
 
    </header>
     <svg height='0' width='0'>
@@ -182,7 +218,7 @@ export default function Home() {
         </linearGradient>
         <linearGradient id='Gradient1' x1='0' x2='0' y1='0' y2='1'>
           <stop offset='0%' stopColor={palette[2]}  />
-          <stop offset='100%' stopColor={palette[3]}  />
+          <stop offset='100%' stopColor={palette[3]} />
         </linearGradient>
         <linearGradient id='Gradient2' x1='0' x2='0' y1='0' y2='1'>
           <stop offset='0%' stopColor={palette[4]}  />
@@ -209,32 +245,48 @@ export default function Home() {
           <stop offset='100%' stopColor={palette[15]}  />
         </linearGradient>
         <linearGradient id='Gradient8' x1='0' x2='0' y1='0' y2='1'>
-          <stop offset='0%' stopColor={palette[0]}  />
+          <stop offset='0%' stopColor={palette[1]}  />
+          <stop offset='100%' stopColor={palette[8]}  />
+        </linearGradient>
+        <linearGradient id='Gradient9' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[2]}  />
+          <stop offset='100%' stopColor={palette[9]}  />
+        </linearGradient>
+        <linearGradient id='Gradient10' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[3]}  />
+          <stop offset='100%' stopColor={palette[10]}  />
+        </linearGradient>
+        <linearGradient id='Gradient11' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[4]}  />
+          <stop offset='100%' stopColor={palette[11]}  />
+        </linearGradient>
+        <linearGradient id='Gradient12' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[5]}  />
+          <stop offset='100%' stopColor={palette[12]}  />
+        </linearGradient>
+        <linearGradient id='Gradient13' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[6]}  />
+          <stop offset='100%' stopColor={palette[13]}  />
+        </linearGradient>
+        <linearGradient id='Gradient14' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[7]}  />
+          <stop offset='100%' stopColor={palette[14]}  />
+        </linearGradient>
+        <linearGradient id='Gradient15' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[8]}  />
           <stop offset='100%' stopColor={palette[15]}  />
+        </linearGradient>
+        <linearGradient id='Gradient16' x1='0' x2='0' y1='0' y2='1'>
+          <stop offset='0%' stopColor={palette[randomInt(0,15)]}  />
+          <stop offset='100%' stopColor={palette[randomInt(0,15)]}  />
         </linearGradient>
       </defs>
     </svg>    
-    <svg id='canvas' height={height} viewBox={'0 0 '+width+ ' '+height} width={width} style={{ transition: 'all 1s ease-in', zIndex: -9, background: bgColor, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, minHeight: '100%', minWidth: '100%', height: '4096px', width: '4096px' }}>
-    <g style={{display: 'none'}}>
-      <rect x={0} y={0} height={32} width={width/16} style={{ fill: palette[0] }} />
-      <rect x={width / 16} y={0} height={32} width={width/16} style={{ fill: palette[1] }} />
-      <rect x={width / 16 * 2} y={0} height={32} width={width/16} style={{ fill: palette[2] }} />
-      <rect x={width / 16 * 3} y={0} height={32} width={width/16} style={{ fill: palette[3] }} />
-      <rect x={width / 16 * 4} y={0} height={32} width={width/16} style={{ fill: palette[4] }} />
-      <rect x={width / 16 * 5} y={0} height={32} width={width/16} style={{ fill: palette[5] }} />
-      <rect x={width / 16 * 6} y={0} height={32} width={width/16} style={{ fill: palette[6] }} />
-      <rect x={width / 16 * 7} y={0} height={32} width={width/16} style={{ fill: palette[7] }} />
-      <rect x={width / 16 * 8} y={0} height={32} width={width/16} style={{ fill: palette[8] }} />
-      <rect x={width / 16 * 9} y={0} height={32} width={width/16} style={{ fill: palette[9] }} />
-      <rect x={width / 16 * 10} y={0} height={32} width={width/16} style={{ fill: palette[10] }} />
-      <rect x={width / 16 * 11} y={0} height={32} width={width/16} style={{ fill: palette[11] }} />
-      <rect x={width / 16 * 12} y={0} height={32} width={width/16} style={{ fill: palette[12] }} />
-      <rect x={width / 16 * 13} y={0} height={32} width={width/16} style={{ fill: palette[13] }} />
-      <rect x={width / 16 * 14} y={0} height={32} width={width/16} style={{ fill: palette[14] }} />
-      <rect x={width / 16 * 15} y={0} height={32} width={width/16} style={{ fill: palette[15] }} />
-    </g>
-      <rect style={{ transition: 'all 1s ease-in', x:0,  y: 0, height: height, width: width,  fill: 'url(#Gradient7)'}} />
-      {[...Array(512)].map((x,i) =>
+    <svg id='canvas' height={height} viewBox={'0 0 '+width+ ' '+height} width={width} style={{ transition: 'all 1s ease-in', zIndex: -9, background: bgColor, position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, minHeight: '100%', minWidth: '100%', height: height+'px', width: width+'px' }}>
+      <rect style={{ transition: 'all 1s ease-in', x:0,  y: 0, height: height, width: width, 
+          fill: 'url(#Gradient'+randomInt(0,16)+')'
+      }} />
+      {[...Array(rows)].map((x,i) =>
         <line x1='0' x2={width}  
         y1={Number(i) * Number(gridUnit)} stroke='black' 
         y2={Number(i) * Number(gridUnit)} stroke='black' 
@@ -242,7 +294,7 @@ export default function Home() {
         className='transitions'
         />
       )}
-      {[...Array(512)].map((x,i) =>
+      {[...Array(cols)].map((x,i) =>
         <line x1={i * gridUnit} x2={i * gridUnit}  
         y1={0} stroke='black' 
         y2={height} stroke='black' 
@@ -250,8 +302,96 @@ export default function Home() {
         className='transitions'
         />
       )}
+    <g style={{display: 'block'}}>
+      <rect x={0} y={0} height={16} width={width/16} style={{ fill: palette[0] }} />
+      <rect x={width / 16} y={0} height={16} width={width/16} style={{ fill: palette[1] }} />
+      <rect x={width / 16 * 2} y={0} height={16} width={width/16} style={{ fill: palette[2] }} />
+      <rect x={width / 16 * 3} y={0} height={16} width={width/16} style={{ fill: palette[3] }} />
+      <rect x={width / 16 * 4} y={0} height={16} width={width/16} style={{ fill: palette[4] }} />
+      <rect x={width / 16 * 5} y={0} height={16} width={width/16} style={{ fill: palette[5] }} />
+      <rect x={width / 16 * 6} y={0} height={16} width={width/16} style={{ fill: palette[6] }} />
+      <rect x={width / 16 * 7} y={0} height={16} width={width/16} style={{ fill: palette[7] }} />
+      <rect x={width / 16 * 8} y={0} height={16} width={width/16} style={{ fill: palette[8] }} />
+      <rect x={width / 16 * 9} y={0} height={16} width={width/16} style={{ fill: palette[9] }} />
+      <rect x={width / 16 * 10} y={0} height={16} width={width/16} style={{ fill: palette[10] }} />
+      <rect x={width / 16 * 11} y={0} height={16} width={width/16} style={{ fill: palette[11] }} />
+      <rect x={width / 16 * 12} y={0} height={16} width={width/16} style={{ fill: palette[12] }} />
+      <rect x={width / 16 * 13} y={0} height={16} width={width/16} style={{ fill: palette[13] }} />
+      <rect x={width / 16 * 14} y={0} height={16} width={width/16} style={{ fill: palette[14] }} />
+      <rect x={width / 16 * 15} y={0} height={16} width={width/16} style={{ fill: palette[15] }} />
+    </g>
+      {(generatedDesignCount > 1 && generatedDesignCount < 24) &&
+        <>
+          {[...Array(generatedDesignCount)].map((x,i) =>
+            <line x1={coords[i].x1} y1={coords[i].y1} x2={coords[i].x2} y2={coords[i].y2} stroke={palette[palette.length % i+1]} strokeWidth={strokeWidth} 
+            className='transitions' />
+           )}
+        </>
+      }
+      {(generatedDesignCount > 12 && generatedDesignCount < 24) &&
+        <>
+          {[...Array(generatedDesignCount - 12)].map((x,i) =>
+            <circle cx={coords[i].x1} cy={coords[i].y1} r={randomInt(0,128)} stroke={palette[palette.length % i+1]} strokeWidth={strokeWidthArray[randomInt(0,strokeWidthArray.length-1)]} 
+          //fill={'url(#Gradient'+randomInt(0,16)+')'}
+          fill='transparent'
+            className='transitions' />
+           )}
+        </>
+      }
+      {(generatedDesignCount > 16 && generatedDesignCount < 24) &&
+        <>
+          {[...Array(generatedDesignCount - 12)].map((x,i) =>
+            <rect x={coords[i+4].x1} y={coords[i+4].y1} width={randomInt(0,128)} height={randomInt(0,128)} stroke={palette[palette.length % i+1]} strokeWidth={strokeWidthArray[randomInt(0,strokeWidthArray.length-1)]} 
+          //fill={'url(#Gradient'+randomInt(0,16)+')'}
+          fill='transparent'
+            className='transitions' />
+           )}
+        </>
+      }
     </svg>
+    <footer style={{ position: 'absolute', bottom: 0, left: 0, right: 0, background: 'transparent', padding: '16px', display: 'flex', justifyContent: 'center', gap: '2rem' }}>
+      {generatedDesignCount % 8 === 0 &&
+        <>
+          {actions[randomInt(0,actions.length-1)]}
+          {actions[randomInt(0,actions.length-1)]}
+        </>
+      }
+    </footer>
       </main>
     </>
   )
+}
+
+
+
+// Hook
+function useWindowSize() {
+  // Initialize state with undefined width/height so server and client renders match
+  // Learn more here: https://joshwcomeau.com/react/the-perils-of-rehydration/
+  const [windowSize, setWindowSize] = useState({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    // only execute all the code below in client side
+    // Handler to call on window resize
+    function handleResize() {
+      // Set window width/height to state
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+    
+    // Add event listener
+    window.addEventListener("resize", handleResize);
+     
+    // Call handler right away so state gets updated with initial window size
+    handleResize();
+    
+    // Remove event listener on cleanup
+    return () => window.removeEventListener("resize", handleResize);
+  }, []); // Empty array ensures that effect is only run on mount
+  return windowSize;
 }
