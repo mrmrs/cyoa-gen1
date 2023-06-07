3327180d6888801fbb26b7a63056264e1ea19506
mrmrs <hi@mrmrs.cc>
2023-06-06 18:39:29 -0700

    Improves filters and blurs

--- components/pemrose-grid.js
+++ components/pemrose-grid.js
@@ -5,25 +5,10 @@ import randomColor from 'random-hex-color'
 import { randomInt} from '../lib/random'
 
 
-function generateRandomStrokeDashArray() {
-  const numSegments = Math.floor(Math.random() * 8) + 2; // Generate a random number of segments between 3 and 7
-  const maxValue = 500; // Maximum length for each segment
-  const dashArray = [];
-
-  for (let i = 0; i < numSegments; i++) {
-    const segmentLength = Math.random() * maxValue;
-    dashArray.push(segmentLength.toFixed(2));
-  }
-
-  return dashArray.join(', ');
-}
-
 const ShapeGrid = ({ palette, gap = 16, width = 64, height = 64, lines = 16, xOffset = 0, yOffset = 0, colors, bgColor, color, strokeWidth, cols, rows, fill}) => {
     const unit = (width - strokeWidth) / (lines -1) 
     const size = width / cols - strokeWidth * 2
     const fillBool = randomInt(0,100)
-    
-    
 
     return (
       <>
@@ -36,7 +21,7 @@ const ShapeGrid = ({ palette, gap = 16, width = 64, height = 64, lines = 16, xOf
               r={randomInt(0,parseInt(width/cols/2 - 1))} 
               fill={fillBool > 70? fill : 'url(#Gradient'+randomInt(0,15)+')'}
               strokeWidth={fillBool > 50? 1 : 0}
-              style={{ filter: fillBool < 90? 'none' : randomInt(0,100) > 50? 'url(#displacementFilter)' : 'url(#blurMe)' }}
+              style={{ filter: fillBool < 90? 'none' : randomInt(0,100) > 50? 'url(#displacementFilter)' : 'none' }}
               />
               <rect x={j * (width / cols) + strokeWidth} y={i * height / rows + strokeWidth * 2} height={height/rows - strokeWidth*2} width={width/cols - strokeWidth * 2} strokeWidth={1} fill='transparent' style={{ zIndex: -9999}}/>
             </g>
